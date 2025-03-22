52f0eded2f367dad197b309d0cecc77118f0e813
Hiroyuki Ootomo <hootomo@nvidia.com>
2025-03-21 22:05:54 -0700

    Fix knn_graph type

--- cpp/include/cuvs/neighbors/cagra_graph.hpp
+++ cpp/include/cuvs/neighbors/cagra_graph.hpp
@@ -14,12 +14,12 @@ void build_knn_graph(raft::resources const& res,
                      cuvs::neighbors::nn_descent::index_params build_params);
 
 void optimize(raft::resources const& res,
-              raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+              raft::host_matrix_view<const uint32_t, int64_t, raft::row_major> knn_graph,
               raft::host_matrix_view<uint32_t, int64_t, raft::row_major> new_graph,
               const bool guarantee_connectivity = false);
 
 void optimize(raft::resources const& res,
-              raft::device_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+              raft::device_matrix_view<const uint32_t, int64_t, raft::row_major> knn_graph,
               raft::host_matrix_view<uint32_t, int64_t, raft::row_major> new_graph,
               const bool guarantee_connectivity = false);
 
