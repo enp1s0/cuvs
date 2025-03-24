c01ccd6134c67cb1f1abcb78e1938e1660a93db8
Hiroyuki Ootomo <hootomo@nvidia.com>
2025-03-23 21:05:01 -0700

    Add (u)int8 support

--- cpp/src/neighbors/cagra_build_uint8.cu
+++ cpp/src/neighbors/cagra_build_uint8.cu
@@ -19,21 +19,36 @@
 
 namespace cuvs::neighbors::cagra {
 
-#define RAFT_INST_CAGRA_BUILD(T, IdxT)                                            \
-  auto build(raft::resources const& handle,                                       \
-             const cuvs::neighbors::cagra::index_params& params,                  \
-             raft::device_matrix_view<const T, int64_t, raft::row_major> dataset) \
-    ->cuvs::neighbors::cagra::index<T, IdxT>                                      \
-  {                                                                               \
-    return cuvs::neighbors::cagra::build<T, IdxT>(handle, params, dataset);       \
-  }                                                                               \
-                                                                                  \
-  auto build(raft::resources const& handle,                                       \
-             const cuvs::neighbors::cagra::index_params& params,                  \
-             raft::host_matrix_view<const T, int64_t, raft::row_major> dataset)   \
-    ->cuvs::neighbors::cagra::index<T, IdxT>                                      \
-  {                                                                               \
-    return cuvs::neighbors::cagra::build<T, IdxT>(handle, params, dataset);       \
+#define RAFT_INST_CAGRA_BUILD(T, IdxT)                                                      \
+  auto build(raft::resources const& handle,                                                 \
+             const cuvs::neighbors::cagra::index_params& params,                            \
+             raft::device_matrix_view<const T, int64_t, raft::row_major> dataset)           \
+    ->cuvs::neighbors::cagra::index<T, IdxT>                                                \
+  {                                                                                         \
+    return cuvs::neighbors::cagra::build<T, IdxT>(handle, params, dataset);                 \
+  }                                                                                         \
+                                                                                            \
+  auto build(raft::resources const& handle,                                                 \
+             const cuvs::neighbors::cagra::index_params& params,                            \
+             raft::host_matrix_view<const T, int64_t, raft::row_major> dataset)             \
+    ->cuvs::neighbors::cagra::index<T, IdxT>                                                \
+  {                                                                                         \
+    return cuvs::neighbors::cagra::build<T, IdxT>(handle, params, dataset);                 \
+  }                                                                                         \
+                                                                                            \
+  void build_knn_graph(raft::resources const& res,                                          \
+                       raft::host_matrix_view<const T, int64_t, raft::row_major> dataset,   \
+                       raft::host_matrix_view<IdxT, int64_t, raft::row_major> knn_graph,    \
+                       cuvs::neighbors::nn_descent::index_params build_params)              \
+  {                                                                                         \
+    cuvs::neighbors::cagra::detail::build_knn_graph(res, dataset, knn_graph, build_params); \
+  }                                                                                         \
+  void build_knn_graph(raft::resources const& res,                                          \
+                       raft::device_matrix_view<const T, int64_t, raft::row_major> dataset, \
+                       raft::host_matrix_view<IdxT, int64_t, raft::row_major> knn_graph,    \
+                       cuvs::neighbors::nn_descent::index_params build_params)              \
+  {                                                                                         \
+    cuvs::neighbors::cagra::detail::build_knn_graph(res, dataset, knn_graph, build_params); \
   }
 
 RAFT_INST_CAGRA_BUILD(uint8_t, uint32_t);
