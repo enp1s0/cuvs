5ae76b15ffc89b97a6f03e61f6488fa1577c6b39
Hiroyuki Ootomo <hootomo@nvidia.com>
2025-03-23 22:26:41 -0700

    Add (u)int8 APIs

--- cpp/include/cuvs/neighbors/cagra_graph.hpp
+++ cpp/include/cuvs/neighbors/cagra_graph.hpp
@@ -13,6 +13,26 @@ void build_knn_graph(raft::resources const& res,
                      raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
                      cuvs::neighbors::nn_descent::index_params build_params);
 
+void build_knn_graph(raft::resources const& res,
+                     raft::device_matrix_view<const int8_t, int64_t, raft::row_major> dataset,
+                     raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+                     cuvs::neighbors::nn_descent::index_params build_params);
+
+void build_knn_graph(raft::resources const& res,
+                     raft::host_matrix_view<const int8_t, int64_t, raft::row_major> dataset,
+                     raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+                     cuvs::neighbors::nn_descent::index_params build_params);
+
+void build_knn_graph(raft::resources const& res,
+                     raft::device_matrix_view<const uint8_t, int64_t, raft::row_major> dataset,
+                     raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+                     cuvs::neighbors::nn_descent::index_params build_params);
+
+void build_knn_graph(raft::resources const& res,
+                     raft::host_matrix_view<const uint8_t, int64_t, raft::row_major> dataset,
+                     raft::host_matrix_view<uint32_t, int64_t, raft::row_major> knn_graph,
+                     cuvs::neighbors::nn_descent::index_params build_params);
+
 void optimize(raft::resources const& res,
               raft::host_matrix_view<const uint32_t, int64_t, raft::row_major> knn_graph,
               raft::host_matrix_view<uint32_t, int64_t, raft::row_major> new_graph,
